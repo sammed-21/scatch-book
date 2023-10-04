eadd019b1e7c3d82bf410d2899e752effa43ac0e
sammed-21 <sammed2110@gmail.com>
2023-10-04 21:57:25 +0530

    Add UI of toolbox

---
+++ src/components/Toolbox/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React, { ChangeEvent } from "react";
+import styles from "./index.module.css";
+import { COLORS } from "@/constants";
+const Toolbox = () => {
+  function updateBrushSize(event: ChangeEvent<HTMLInputElement>): void {
+    console.log(event.target.value);
+  }
+
+  return (
+    <div className={styles.toolboxContainer}>
+      <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Stroke Color</h4>
+        <div className={styles.itemContainer}>
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.BLACK }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.BLACK }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.RED }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.GREEN }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.BLUE }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.ORANGE }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.YELLOW }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.WHITE }}
+          />
+        </div>
+      </div>
+      <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Brush size</h4>
+        <div className={styles.itemContainer}>
+          <input
+            type="range"
+            min={1}
+            max={10}
+            step={1}
+            onChange={updateBrushSize}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Toolbox;
