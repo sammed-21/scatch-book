3a533711406a505ea4d18c6a857d1f4ca1959df2
sammed-21 <sammed2110@gmail.com>
2023-10-08 07:33:32 +0530

    add the env variable in socket file

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,10 @@
 import { io } from "socket.io-client";
  
-const URL = process.env.NODE_ENV === 'production' ? 'https://scatch-book-server.onrender.com' : 'http://localhost:5000'
-export const socket = io(URL);
+const apiUrl = process.env.API_URL;
+const nodeEnv = process.env.NODE_ENV;
+ console.log(nodeEnv)
+//  console.log(process.env.NODE_ENV)
+const URL = nodeEnv != 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
+
+console.log(URL)
+export const socket = io(URL);  
