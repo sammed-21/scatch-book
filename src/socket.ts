283e7c531302a02cdaabf8023677383106c5fe8b
sammed-21 <sammed2110@gmail.com>
2023-10-08 08:44:45 +0530

    fix the url mismatch on socket file

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
- 
-const apiUrl = process.env.API_URL;
+  
 const nodeEnv = process.env.NODE_ENV;
- console.log(nodeEnv)
-//  console.log(process.env.NODE_ENV)
+ 
 const URL = nodeEnv === 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
-
-console.log(URL)
+ 
 export const socket = io(URL);  
