ce0f6f9a6cf95ddfed3ce0b0714ca229cc79b010
sammed-21 <sammed2110@gmail.com>
2023-10-08 11:37:34 +0530

    provide BE endpoint

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
   
 const nodeEnv = process.env.NODE_ENV;
- 
-const URL = nodeEnv === 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
- 
+ const URL = process.env.NODE_ENV === 'production'?'https://scatch-book-server.onrender.com':'http://localhost:5000'
+// const URL = nodeEnv === 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
+ console.log(URL)
 export const socket = io(URL);  
