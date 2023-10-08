653bbcf49e1e69b0edddd38cd5acefdf755398cf
sammed-21 <sammed2110@gmail.com>
2023-10-08 08:17:33 +0530

    fix the error in socket

--- src/socket.ts
+++ src/socket.ts
@@ -4,7 +4,7 @@ const apiUrl = process.env.API_URL;
 const nodeEnv = process.env.NODE_ENV;
  console.log(nodeEnv)
 //  console.log(process.env.NODE_ENV)
-const URL = nodeEnv != 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
+const URL = nodeEnv === 'development' ? 'http://localhost:5000' : 'https://scatch-book-server.onrender.com'
 
 console.log(URL)
 export const socket = io(URL);  
